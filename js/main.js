e58bd983c2a88a04513452a446c3caf1479b0721
Ambar Sethi <ambarrrsethi@gmail.com>
2015-08-13 20:49:14 +0100

    BMi calculator trying to set if funtion for imperial or metric

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
-var answerBox = document.getElementById("basic-answer");
+//Basic Calculator
 
+var answerBox = document.getElementById("basic-answer");
+//Setting answer box to hide
 answerBox.className = "hide";
 
 
 var basicButton = document.getElementById("basic-calc");
+//Setting an Event listener on Click
 
 basicButton.addEventListener("click", function(){
   
@@ -12,6 +15,7 @@ basicButton.addEventListener("click", function(){
   var basicOp = document.getElementById("basic-operation").value;
   var basicAnswer = document.getElementById("basic-answer").value;
 
+//Will carry out function depending on operator chosen by user
   switch(basicOp){
     case "+":
       basicAnswer = basicNumOne + basicNumTwo;
@@ -26,11 +30,43 @@ basicButton.addEventListener("click", function(){
       basicAnswer =  basicNumOne * basicNumTwo;
       break;
   }
+//show the answer box and answer
 answerBox.className = "show"
 var displayAnswer = document.getElementById("basic-answer-alert")
 displayAnswer.innerHTML = "Your answer is: " + basicAnswer;
 
 })
+
+
+//BMI Calculator
+
+var bmiBox = document.getElementById("bmi-answer");
+//Setting answer box to hide
+bmiBox.className = "show";
+
+
+var bmiButton = document.getElementById("bmi-calc");
+//Setting an Event listener on Click
+
+bmiButton.addEventListener("click", function(){
+  
+var bmiMass = parseFloat(document.getElementById("bmi-mass-unit").value);
+var bmiHeight = parseFloat(document.getElementById("bmi-height-unit").value);
+
+
+
+
+
+// var bmiMass, bmiHeight, bmiMass / Math.pow(height, 2)
+
+//   var bmiAnswer = document.getElementById("basic-calc").value;
+
+
+//     //   var mass = parseFloat(prompt("Mass (KG)"));
+//     //   var height = parseFloat(prompt("Height (Meters)"));
+//     //   
+
+
 
 
 
